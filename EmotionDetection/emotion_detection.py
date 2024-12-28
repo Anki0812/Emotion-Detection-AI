56ce0f2af9210c4d9b4ab2cfdfc6605b420ef3cb
Anki0812 <anki0812@gmail.com>
2024-12-28 01:43:51 -0500

    server code

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,12 +1,13 @@
 import requests
 import json
 
+
 def fetch_dominant_emotion(emotions):
-    '''
+    """
     Function to fetch the dominant emotion from the response
     input - emotion dictionary
     output - dominant emotion
-    '''
+    """
     d_score = 0
     d_emotion = None
     for emotion, score in emotions.items():
@@ -17,21 +18,21 @@ def fetch_dominant_emotion(emotions):
 
 
 def emotion_detector(text_to_analyse):
-    '''
+    """
     Function to fetch the list of emotions for user input statement
     input - User input statement
     output - emotions in JSON/Dictionary
-    '''
-    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    myobj = { "raw_document": { "text": text_to_analyse } }
+    """
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    myobj = {"raw_document": {"text": text_to_analyse}}
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
 
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json=myobj, headers=header)
     formatted_response = json.loads(response.text)
     if response.status_code == 200:
-        emotions = formatted_response['emotionPredictions'][0]['emotion']
+        emotions = formatted_response["emotionPredictions"][0]["emotion"]
         dominant_emotion = fetch_dominant_emotion(emotions)
-        emotions['dominant_emotion'] = dominant_emotion        
+        emotions["dominant_emotion"] = dominant_emotion
     else:
-        emotions = {'message':'Emotions not found'}
+        emotions = {"message": "Emotions not found"}
     return emotions
