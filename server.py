4df4977d71aa2818e3e2d04bcbe9c083dac6a6ce
Anki0812 <anki0812@gmail.com>
2024-12-28 02:05:03 -0500

    pylint fix

--- server.py
+++ server.py
@@ -1,12 +1,17 @@
+"""
+The application to check the emotions of the text entered by user
+to be executed over the Flask channel and deployed on
+localhost:5000.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask("Emotion Detection")
 
-
 @app.route("/")
 def render_index_page():
-    """This function initiates the rendering of the main application
+    """
+    This function initiates the rendering of the main application
     page over the Flask channel
     """
     return render_template("index.html")
@@ -14,16 +19,19 @@ def render_index_page():
 
 @app.route("/emotionDetector")
 def sent_analyzer():
-    # Retrieve the text to analyze from the request arguments
+    """
+    This function is used to analyse the emotions of input test by user
+    """
     text_to_analyze = request.args.get("textToAnalyze")
-
     response = emotion_detector(text_to_analyze)
-
     if response["dominant_emotion"]:
-        final_response = f"""For the given statement, the system response is 'anger': {response['anger']}, 
-        'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} 
-        and 'sadness': {response['sadness']}.
-        The dominant emotion is {response['dominant_emotion']}"""
+        final_response = (
+            f"For the given statement, the system response is 'anger': {response['anger']}, "
+            f"'disgust': {response['disgust']}, 'fear': {response['fear']}, "
+            f"'joy': {response['joy']} "
+            f"and 'sadness': {response['sadness']}. "
+            f"The dominant emotion is {response['dominant_emotion']}."
+        )
     else:
         final_response = "Invalid text! Please try again!."
 
